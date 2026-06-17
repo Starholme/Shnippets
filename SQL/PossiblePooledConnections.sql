39d121c00adf491101481dd4d0b278711531a2cc
Russel Delainey <rdelainey@pa911.com>
2026-06-17 14:31:40 -0600

    Add query for checking pooled connection counts

---
+++ SQL/PossiblePooledConnections.sql
@@ -0,0 +1,13 @@
+SELECT des.program_name,
+       des.login_name,
+       des.host_name,
+       COUNT(des.session_id) AS [Connections]
+FROM sys.dm_exec_sessions des
+INNER JOIN sys.dm_exec_connections DEC
+        ON des.session_id = DEC.session_id
+WHERE des.is_user_process = 1
+GROUP BY des.program_name,
+         des.login_name,
+         des.host_name
+HAVING COUNT(des.session_id) > 2
+ORDER BY COUNT(des.session_id) DESC

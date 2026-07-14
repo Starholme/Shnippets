b1ee43866f67297b830702a94b701929d2a46eda
Russel Delainey <rdelainey@pa911.com>
2026-07-14 13:46:44 -0600

    Updated ping logged script to use generic logger, better logging

--- PS_CMD/PingLogged.ps1
+++ PS_CMD/PingLogged.ps1
@@ -1,21 +1,101 @@
 $target = "someserver" # Or an IP address
-$logFile = ".\ping_log.txt"
 $intervalMs = 250 #ms between pings
+$numberOfPings = 0 #number of pings to run before stopping, 0 to run forever
+$slowPingMs = 50 #pings over this number of ms will be logged
 
-$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-"****$timestamp - Ping to $target started****" | Out-File -FilePath $logFile -Append
+###region Configuration###
 
-while ($true) {
-    $result = Test-Connection -ComputerName $target -Count 1 -ErrorAction SilentlyContinue
-    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+#Logging
+#Where do you want the log, and what name? ie: "c:\logs\log.txt"
+$logFilePath = ".\ping_log.txt"
+#How many lines of log do you want to keep? Setting to 0 or 1 will clear the log file on each start.
+$logLength = 10000
 
-    if ($result) {
-        "$timestamp - Ping to $target successful. Latency: $($result.ResponseTime)ms"
-		if ($result.ResponseTime > 50){
-			"$timestamp - Ping to $target slow: $($result.ResponseTime)ms" | Out-File -FilePath $logFile -Append
+###endregion Configuration###
+
+###region Functions###
+
+#region Logging functions
+
+#Writes a string to console and to the log file
+Function Log($someText)
+{
+	Write-Host $someText
+	"$someText" | Out-File -FilePath "$logFilePath" -Append -Encoding Unicode
+}
+
+#Trims the log file to the log length configured
+Function TrimLogFile
+{
+	try{
+		if (Test-Path $logFilePath)
+		{
+			(Get-Content -Tail $logLength -Encoding Unicode $logFilePath )| Set-Content -Encoding Unicode $logFilePath
 		}
-    } else {
-        "$timestamp - Ping to $target failed." | Out-File -FilePath $logFile -Append
-    }
-    Start-Sleep -Milliseconds $intervalMs # Adjust the interval as needed
+		else
+		{
+			New-Item -Path $logFilePath -ItemType File -Force
+		}
+	}
+	catch
+	{
+	}
+}
+
+#Logs and records start of script
+Function LogStart
+{
+	TrimLogFile
+	$global:executionLogStartTime = Get-Date
+	Log("")
+	Log("----Start Processing $global:executionLogStartTime----")
+}
+
+#Logs and records end of script, trims log file
+Function LogEnd
+{
+	$executionEndTime = Get-Date
+	$duration = [int]($executionEndTime-$global:executionLogStartTime).TotalMilliseconds
+	Log("----End Processing $executionEndTime, Duration in ms: $duration----")
+	Log("")
+}
+#endregion Logging functions
+
+$ErrorActionPreference = 'Stop'
+try
+{	
+	if ($numberOfPings -eq 0) {LogStart}
+	TrimLogFile
+
+	$loopCounter = 1
+
+	while ($loopCounter -ne $numberOfPings) {
+		
+		$result = Test-Connection -ComputerName $target -Count 1 -ErrorAction SilentlyContinue
+		$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+
+		if ($result) {
+			"$timestamp - Ping to $target successful. Latency: $($result.ResponseTime)ms"
+			if ($result.ResponseTime -gt $slowPingMs){
+				Log "$timestamp - Ping to $target slow: $($result.ResponseTime)ms"
+			}
+		} else {
+			Log "$timestamp - Ping to $target failed."
+		}
+		
+		if ($loopCounter % 10000 -eq 0)
+		{
+			TrimLogFile
+		}
+		Start-Sleep -Milliseconds $intervalMs
+		$loopCounter++
+	}
+}
+catch
+{
+	Log("$($_.Exception.Message)")
+}
+finally
+{
+	if ($numberOfPings -eq 0) {LogEnd}
 }
